f845e72b77635aeaf2c6458e403a1531bde077f3
chushov <work@chushov.com>
2021-09-15 13:34:27 +0300

    add indexHadler, css styles, routes

--- main.go
+++ main.go
@@ -26,3 +26,22 @@ func main() {
     mux.HandleFunc("/", indexHandler)
     http.ListenAndServe(":"+port, mux)
 }
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+    u, err := url.Parse(r.URL.String())
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte("Internal server error"))
+        return
+    }
+
+    params := u.Query()
+    searchKey := params.Get("q")
+    page := params.Get("page")
+    if page == "" {
+        page = "1"
+    }
+
+    fmt.Println("Search Query is: ", searchKey)
+    fmt.Println("Results page is: ", page)
+}
